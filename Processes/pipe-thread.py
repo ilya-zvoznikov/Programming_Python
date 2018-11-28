530cada5c5dd9992a16842c51d594c1475c1e68c
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2018-11-28 18:11:28 +0700

    page 326

---
+++ Processes/pipe-thread.py
@@ -0,0 +1,19 @@
+"""анонимные каналы и потоки выполнения вместо процессов;
+эта версия работает и в Windows"""
+
+import os, time, threading
+
+
+def child(pipeout):
+    zzz = 0
+    while True:
+        time.sleep(zzz)
+        msg = ('Spam %03d' % zzz).encode()
+        os.write(pipeout, msg)
+        zzz = (zzz + 1) % 5
+
+
+def parent(pipein):
+    while True:
+        line = os.read(pipein, 32)
+        print('Parent %d got [%s] at %s' % (os.getpid(), line, time.time()))
