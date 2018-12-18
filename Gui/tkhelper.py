18e9d333b074af415b6f762de34ead8d5c5d7c56
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2018-12-18 17:35:00 +0700

    page 680

---
+++ Gui/tkhelper.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+
+def tkprinter(widgetname, full=False, width=25):
+    print()
+    print('WIDGET NAME: %s' % str(widgetname.__name__))
+    length = '{:^' + str(width) + '}'
+    f = 6 if full else 1
+    print('-' * int(length[3:-1]) * f)
+    print((length * f).format('OPTION', 'NAME', 'NAME for DB', 'CLASS for DB', 'DEFAULT VALUE', 'CURRENT VALUE'))
+    print('-' * int(length[3:-1]) * f)
+    w = widgetname()
+    for key in w.config():
+        print(length.format(key), end='')
+        if full:
+            for elem in w.config()[key]:
+                print(length.format(str(elem)), end='')
+        print()
+
+
+if __name__ == '__main__':
+    tkprinter(Button)
