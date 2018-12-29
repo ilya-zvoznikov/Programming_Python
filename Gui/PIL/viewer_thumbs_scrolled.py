e5282a17db92a3fbef2573a16662d122b2a0c6d9
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2018-12-29 18:40:01 +0700

    page 721

--- Gui/PIL/viewer_thumbs_scrolled.py
+++ Gui/PIL/viewer_thumbs_scrolled.py
@@ -35,4 +35,32 @@ def viewer(imgdir, kind=Toplevel, numcols=None, height=300, width=300):
     quit = Button(win, text='Quit', command=win.quit, bg='beige')
     quit.pack(side=BOTTOM, fill=X)
 
-# ПРОГРАММА НЕ ЗАКОНЧЕНА!!!
+    canvas = Canvas(win, borderwidth=0)
+    vbar = Scrollbar(win)
+    hbar = Scrollbar(win, orient='horizontal')
+
+    vbar.pack(side=RIGHT, fill=Y)
+    hbar.pack(side=BOTTOM, fill=X)
+    canvas.pack(side=TOP, expand=YES, fill=BOTH)
+
+    vbar.config(command=canvas.yview)
+    hbar.config(command=canvas.xview)
+    canvas.config(yscrollcommand=vbar.set)
+    canvas.comfig(xscrollcommand=hbar.set)
+    canvas.config(height=height, width=width)
+    thumbs = makeThumbs(imgdir)
+    numthumbs = len(thumbs)
+    if not numcols:
+        numcols = int(math.ceil(math.sqrt(numthumbs)))
+    numrows = int(math.ceil(numthubs / numcols))
+
+    linksize = max(thumbs[0][1].size)
+    fullsize = (0, 0,                                # координаты верхнего левого угла
+        (linksize * numcols), (linksize * numrows))  # координаты нижнего правого угла
+    canvas.config(scrollregion=fullsize)
+
+    rowpos = 0
+    savephotos = []
+
+    while thumbs:
+        thumbsrow, thumbs = thumbs[:numcols], thumbs[numcols:]
